6d832845b4bf9d50431cb7a9ea7fd5945c5dc9e0
Philipp Mayer <mayerph@stud.sbg.ac.at>
2019-03-12 13:51:38 +0100

    first part of structure setup

---
+++ util/points.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <iostream> // for endl
+#include "points.h"
+
+// simple function to display points in a Point vector.
+void pdisplay (const std::vector<Point>& p) {
+  for (unsigned int i = 0; i < p.size(); i++) {
+    std::cout << p[i] << std::endl;
+  }
+}
