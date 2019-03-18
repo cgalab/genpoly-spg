369fead3ed313bcf337bc0f1d839787a6d77b4cb
Philipp Mayer <pmayer@cosy.sbg.ac.at>
2019-03-18 15:24:25 +0100

    added preallocation of memory for vector

--- util/includes/polygon.h
+++ util/includes/polygon.h
@@ -6,13 +6,14 @@
 #define __POLYGON_H_
 
 class Polygon{
-  public:
-    std::vector<Point*> vertices;
-
-    void addVertex(Point* v){ vertices.push_back(v);}
-    int getNumberOfVertices(){ return vertices.size();}
-    Point* getVertex(int i){ return vertices[i]; }
-    void replaceVertex(Point* v, int i){ vertices[i] = v;}
+	public:
+   	std::vector<Point*> vertices;
+		
+		Polygon(int n){ vertices.reserve(n); }
+   	void addVertex(Point* v){ vertices.push_back(v);}
+   	int getNumberOfVertices(){ return vertices.size();}
+   	Point* getVertex(int i){ return vertices[i]; }
+   	void replaceVertex(Point* v, int i){ vertices[i] = v;}
 
 };
 
