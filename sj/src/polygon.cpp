2fe9bd3924eb642d3ac82dcb25d61ba2c8cd9a34
Steinthor Jasonarson <sjas@cs.sbg.ac.at>
2019-04-23 10:39:50 +0200

    Slight cleanup of Area code.

--- sj/src/polygon.cpp
+++ sj/src/polygon.cpp
@@ -3,23 +3,20 @@
 
 double pol_calc_area(std::vector<unsigned int>& polygon, std::vector<Point>& points) {
   double Area = 0;
-  Point p1, p2;
+  Point prev, p;
   unsigned int index;
 
+  prev = points[polygon[polygon.size()-1]];
   // for each vertex in the polygon, index is the latter point
-  for (index = 1; index < polygon.size(); ++index)
+  for (index = 0; index < polygon.size(); ++index)
   {
-    p1 = points[polygon[index-1]];
-    p2 = points[polygon[index]];
+    p = points[polygon[index]];
 
-    Area += p1.x * p2.y - p1.y * p2.x;
+    Area += p.x * prev.y - p.y * prev.x;
+    prev = p;
   }
-  // edge between last point and first point
-  p1 = points[polygon[0]];
-  p2 = points[polygon[polygon.size()-1]];
 
-  Area += p1.x * p2.y - p1.y * p2.x;
-  Area = Area /2;
+  Area = Area / 2;
 
   return Area;
 }
