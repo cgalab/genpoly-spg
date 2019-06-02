0df1f4fb61896da1cd053042d364cbfc93e07fca
Steinthor Jasonarson <sjas@cosy.sbg.ac.at>
2019-06-02 19:47:27 +0200

    no comment.

--- sj/src/curve.cpp
+++ sj/src/curve.cpp
@@ -1,5 +1,6 @@
 #include <iostream> // for endl
 #include <vector>
+#include <assert.h>
 #include "basicDefinitions.h"
 #include "point.h"
 #include "edge.h"
@@ -7,6 +8,14 @@
 
 enum error curve(std::vector<unsigned int>& polygon, std::vector<Point>& points, unsigned int randseed) {
   ++randseed;
-  std::cerr << "hello world: pol.size: " << polygon.size() << "points: " << points[0] << ", seed: " << randseed << std::endl;
+
+  //start with creating a vector for the lexicographically sorted indexes of 'points'
+  std::vector<unsigned int> lex (points.size());
+  fill_lex(lex, points); // fill 'lex' with the indexes
+
+  std::cerr << "first point: " << points[lex[0]] << ", pol: " << polygon.size() << std::endl;
+
+
+
   return UNEXPECTED_ERROR;
 }
