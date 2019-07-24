ef3f5659787d9b2e8babd5d872880d57c4b6ee98
Steinþór Jasonarson <sjas@cosy.sbg.ac.at>
2019-07-24 13:31:10 +0200

    cleanup.

--- sj/includes/opt2.h
+++ sj/includes/opt2.h
@@ -8,9 +8,6 @@
 #include "point.h"
 #include "edge.h"
 
-bool sortbysec(const std::pair<Point,double> &a, const std::pair<Point,double> &b);
-bool sortbyint(const int i, const int j);
-enum edge_t edgeCheck(unsigned int& index, Edge& e1, std::list<Edge>& edges, std::vector<unsigned int>& polygon, std::vector<Point>& points);
 enum error opt2(std::vector<unsigned int>& polygon, std::vector<Point>& points, unsigned int randseed);
 
 #endif
