755f64fde9f53fbf24a850aff77138c09266b104
Steinthor Jasonarson <sjas@cs.sbg.ac.at>
2019-08-15 16:49:15 +0200

    cleanup, functions moved to 'opt2base'

--- sj/includes/opt2.h
+++ sj/includes/opt2.h
@@ -2,13 +2,10 @@
 #define __2OPT_H_
 
 #include <vector>
-#include <utility> // for std::pair
-#include <list>
 #include "basicDefinitions.h"
 #include "point.h"
 #include "edge.h"
 
-enum edge_t removeEdgeFromSet(Edge& e, std::set<Edge>& edgeS, std::vector<unsigned int>& polygon, std::vector<Point>& points);
 enum error opt2(std::vector<unsigned int>& polygon, std::vector<Point>& points, unsigned int randseed);
 
 #endif
--- sj/includes/opt2c.h
+++ sj/includes/opt2c.h
@@ -2,16 +2,10 @@
 #define __2OPTC_H_
 
 #include <vector>
-#include <utility> // for std::pair
-#include <list>
 #include "basicDefinitions.h"
 #include "point.h"
 #include "edge.h"
 
-void update_lowest_index_c(Edge e1, Edge e2, unsigned int& lowest_index);
-void update_lowest_index_c(Point *a, Point *b, Point *c, unsigned int& lowest_index);
-enum edge_t removeEdgeFromSetc(Edge& e, unsigned int& lowest_index, std::set<Edge>& edgeS, std::vector<unsigned int>& polygon, std::vector<Point>& points);
-std::pair<enum edge_t, std::set<Edge>::iterator> processEdgec(Edge& e, unsigned int& lowest_index, std::set<Edge>& edgeS, std::vector<unsigned int>& polygon, std::vector<Point>& points);
 enum error opt2c(std::vector<unsigned int>& polygon, std::vector<Point>& points, unsigned int randseed);
 
 #endif
