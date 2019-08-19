f866e317266650a36c8a2854ec46b8c8e9fa5ac5
Philipp Mayer <mayerph@stud.sbg.ac.at>
2019-08-19 18:52:36 +0200

    cleaned the initialPolygonGenerator class up

--- triangulation_approach/includes/initialPolygonGenerator.h
+++ triangulation_approach/includes/initialPolygonGenerator.h
@@ -1,16 +1,74 @@
+/*
+	Include standard libraries
+*/
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+/*
+	Include my headers
+*/
 #include "vertex.h"
 #include "triangulation.h"
 #include "tedge.h"
 #include "triangle.h"
-#include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
 #include "settings.h"
 
-enum class RPShapes {RPS_FLAT, RPS_QUADRATIC};
+/*
+	The function generateRegularPolygon at first generates a new triangulation
+	with the vertices of the start polygon in regular shape, i.e. all vertices 
+	lay on a circle.
+	Afterwards the the start polygon is triangulated, i.e. the polygon edges are
+	added and the inner of the polygon is triangulated, then the polygon is boxed
+	by a square with a triangulation between the polygon and the square.
 
-//Polygon* generateRectangularPolygon(int n, enum RPShapes type);
-Triangulation* generateRegularPolygon();
-void initialTriangulationPseudoStar(Triangulation* T);
-void initialTriangulationZigZag(Triangulation* T);
-void boxPolygon(Triangulation* T, int startIndex);
+	The radius of the start polygon, the number of vertices and the size of the
+	box are globally defined in settings.
+
+	@return		The generated triangulation
+*/
+Triangulation *generateRegularPolygon();
+
+/*
+	The function initialTriangulationPseudoStar adds the polygon edges to the
+	vertices of a regular shaped polygon with no holes. Then it moves the
+	vertex 0 to the center of the circle and triangulates the inner of the
+	polygon by connecting each other vertex to the vertex in the center.
+
+	Note:
+	In principle this approach would work, but it gets very slow for larger
+	initial polygons as each vertex shares an edge with the one center vertex
+	so the edges list of the center vertex becomes very slow.
+
+	@param	T 	The triangulation containing the already generated but not
+				linked vertices of the start polygon
+*/
+void initialTriangulationPseudoStar(Triangulation *T);
+
+/*
+	The function initialTriangulationZigZag adds the polygon edges to the
+	vertices of a regular shaped polygon with no holes and triangulates the
+	inner of the polygon in zig-zag style (for more information on the zig-zag
+	style take a look at my master thesis).
+
+	Note:
+	The suitable startindex for the box polygon function is 0!
+
+	@param	T 	The triangulation containing the already generated but not
+				linked vertices of the start polygon
+*/
+void initialTriangulationZigZag(Triangulation *T);
+
+/*
+	The function boxPolygon generates a square around the polygon and
+	triangulates the area between the polygon and the square.
+
+	@param	T 			The triangulation containing the polygon and the 
+						triangulation inside the polygon
+	@param	startIndex 	The index of the first vertex being placed at the circle
+						of vertices (normally each vertex is placed at the circle
+						so the startindex can be zero, but for some special inner
+						triangulations, e.g. pseudoStar, the vertex 0 is not at 
+						the	circle)
+*/
+void boxPolygon(Triangulation *T, int startIndex);
--- triangulation_approach/src/initialPolygonGenerator.cpp
+++ triangulation_approach/src/initialPolygonGenerator.cpp
@@ -1,35 +1,61 @@
 #include "initialPolygonGenerator.h"
 
-Triangulation* generateRegularPolygon(){
-	double alpha;
+/*
+	The function generateRegularPolygon at first generates a new triangulation
+	with the vertices of the start polygon in regular shape, i.e. all vertices 
+	lay on a circle.
+	Afterwards the the start polygon is triangulated, i.e. the polygon edges are
+	added and the inner of the polygon is triangulated, then the polygon is boxed
+	by a square with a triangulation between the polygon and the square.
+
+	The radius of the start polygon, the number of vertices and the size of the
+	box are globally defined in settings.
+
+	@return		The generated triangulation
+*/
+Triangulation *generateRegularPolygon(){
+	double alpha; // The angular distance between the vertices
 	int i;
-	Vertex* v;
+	Vertex *v;
 	Triangulation* T = new Triangulation(Settings::targetSize);
 	
 	alpha = 2 * M_PI / Settings::initialSize;
 
+	// Distribute the vertices equally on a circle
 	for(i = 0; i < Settings::initialSize; i++){
 		v = new Vertex(Settings::radius * cos(i * alpha), Settings::radius * sin(i * alpha));
 		(*T).addVertex(v);
 	}
 
+	// Triangulate the inner of the polygon in zig-zag style
 	initialTriangulationZigZag(T);
 
+	// Box the polygon by a square and triangulate the area between the square and the polygon
 	boxPolygon(T, 0);
 
-	return T; 
-	
+	return T; 	
 }
 
-// boxPolygon startindex 1
-// works in principle, but is slow as shit
+/*
+	The function initialTriangulationPseudoStar adds the polygon edges to the
+	vertices of a regular shaped polygon with no holes. Then it moves the
+	vertex 0 to the center of the circle and triangulates the inner of the
+	polygon by connecting each other vertex to the vertex in the center.
+
+	Note:
+	In principle this approach would work, but it gets very slow for larger
+	initial polygons as each vertex shares an edge with the one center vertex
+	so the edges list of the center vertex becomes very slow.
+
+	@param	T 	The triangulation containing the already generated but not
+				linked vertices of the start polygon
+*/
 void initialTriangulationPseudoStar(Triangulation* T){
 	int i;
 	Vertex *center, *v0, *v1;
 	TEdge *e0, *e1, *e2, *start;
-	bool ok = true;
 
-	// move vertex 0 to the origin
+	// Move vertex 0 to the origin
 	center = (*T).getVertex(0);
 	(*center).setPosition(0, 0);
 
@@ -43,7 +69,7 @@ void initialTriangulationPseudoStar(Triangulation* T){
 	(*T).addEdge(e1);
 	(*T).addEdge(start);
 
-	new Triangle(e0, e1, start, v0, v1, center, "initialGeneratorStart", ok);
+	new Triangle(e0, e1, start, v0, v1, center);
 
 	for(i = 3; i < Settings::initialSize; i++){
 		v0 = v1;
@@ -56,30 +82,42 @@ void initialTriangulationPseudoStar(Triangulation* T){
 		(*T).addEdge(e0);
 		(*T).addEdge(e1);
 
-		new Triangle(e0, e1, e2, v0, v1, center, "initialGenerator1", ok);
+		new Triangle(e0, e1, e2, v0, v1, center);
 	}
 
+	// At the end we have to insert a triangle outside of the polygon such that
+	// the triangulation forms the convex hull of the vertices
 	v0 = (*T).getVertex(1);
 	(*e1).setEdgeType(EdgeType::POLYGON);
 	e2 = new TEdge(v0, v1);
 	(*T).addEdge(e2);
-	new Triangle(e1, e2, start, v0, v1, center, "initialGeneratorEnd", ok);
+	new Triangle(e1, e2, start, v0, v1, center);
 }
 
-// boxPolygon startindex 0
-void initialTriangulationZigZag(Triangulation* T){
-	int i;
-	Vertex* v0 = NULL, *v1 = NULL, *v2 = NULL;
-	TEdge* e0 = NULL, *e1 = NULL, *e2 = NULL;
-	bool ok;
 
-	// the inital triangulation contains n-2 triangles
+/*
+	The function initialTriangulationZigZag adds the polygon edges to the
+	vertices of a regular shaped polygon with no holes and triangulates the
+	inner of the polygon in zig-zag style (for more information on the zig-zag
+	style take a look at my master thesis).
+
+	Note:
+	The suitable startindex for the box polygon function is 0!
+*/
+void initialTriangulationZigZag(Triangulation *T){
+	int i;
+	Vertex *v0 = NULL, *v1 = NULL, *v2 = NULL;
+	TEdge *e0 = NULL, *e1 = NULL, *e2 = NULL;
+
+	// The first edge connects vertex 0 and vertex n-1 to close the circle
 	v0 = (*T).getVertex(0);
 	v1 = (*T).getVertex(Settings::initialSize - 1);
 	e0 = new TEdge(v1, v0, EdgeType::POLYGON);
 	(*T).addEdge(e0);
+
+	// The inital triangulation contains n-2 triangles
 	for(i = 0; i < Settings::initialSize - 2; i++){
-		ok = true;
+
 		if(i % 2 == 0){
 			// v0 stays as it is and v1 becomes v2
 			v2 = v1;
@@ -93,7 +131,8 @@ void initialTriangulationZigZag(Triangulation* T){
 			(*T).addEdge(e0);
 			(*T).addEdge(e1);
 
-			new Triangle(e0, e1, e2, v0, v1, v2, "initialGeneratorZigZag1", ok);
+			new Triangle(e0, e1, e2, v0, v1, v2);
+
 		}else{
 			// v1 becomes v0 and v2 stays as it is
 			v0 = v1;
@@ -107,12 +146,12 @@ void initialTriangulationZigZag(Triangulation* T){
 			(*T).addEdge(e0);
 			(*T).addEdge(e1);
 
-			new Triangle(e0, e1, e2, v0, v1, v2, "initialGeneratorZigZag2", ok);
+			new Triangle(e0, e1, e2, v0, v1, v2);
 		}
-
-		//(*t).print();
 	}
 
+	// To close the polygon it makes a difference whether the number of veritces
+	// is even or odd
 	if(Settings::initialSize % 2 == 0){
 		(*e0).setEdgeType(EdgeType::POLYGON);
 	}else{
@@ -120,26 +159,44 @@ void initialTriangulationZigZag(Triangulation* T){
 	}
 }
 
+/*
+	The function boxPolygon generates a square around the polygon and
+	triangulates the area between the polygon and the square.
+
+	@param	T 			The triangulation containing the polygon and the 
+						triangulation inside the polygon
+	@param	startIndex 	The index of the first vertex being placed at the circle
+						of vertices (normally each vertex is placed at the circle
+						so the startindex can be zero, but for some special inner
+						triangulations, e.g. pseudoStar, the vertex 0 is not at 
+						the	circle)
+*/
 void boxPolygon(Triangulation* T, int startIndex){
-	// vertices and edges of the rectangle
+	// Vertices and edges of the rectangle
 	Vertex *rv0, *rv1, *rv2, *rv3;
 	TEdge *re0, *re1, *re2, *re3;
-	int limit0, limit1, limit2, i;
+	// End indices of one quadrant
+	int limit0, limit1, limit2;
+
+	int i;
 	TEdge *start, *prev, *next;
 	Vertex *v0, *v1;
-	bool ok = false;
 
-	// generate the rectangle
-	/*
+	/* 
+		Generate the rectangle int the following orientation:
+
 		1 - 0
 		|   |
 		2 - 3
 	*/
+	// Vertices
 	rv0 = new Vertex(Settings::boxSize / 2, Settings::boxSize / 2, true);
-	rv1 = new Vertex(-Settings::boxSize / 2, Settings::boxSize / 2, true);
+	rv1 = new Vertex(- Settings::boxSize / 2, Settings::boxSize / 2, true);
 	rv2 = new Vertex(- Settings::boxSize / 2, - Settings::boxSize / 2, true);
 	rv3 = new Vertex(Settings::boxSize / 2, - Settings::boxSize / 2, true);
 	(*T).setRectangle(rv0, rv1, rv2, rv3);
+
+	// Edges
 	re0 = new TEdge(rv0, rv1, EdgeType::FRAME);
 	re1 = new TEdge(rv1, rv2, EdgeType::FRAME);
 	re2 = new TEdge(rv2, rv3, EdgeType::FRAME);
@@ -149,12 +206,12 @@ void boxPolygon(Triangulation* T, int startIndex){
 	(*T).addEdge(re2);
 	(*T).addEdge(re3);
 
-	// studieren geht über probieren :D
+	// Calculate the last vertex for each quadrant
 	limit0 = (Settings::initialSize + 1) / 4;
 	limit1 = Settings::initialSize / 2;
 	limit2 = 3 * Settings::initialSize / 4;
 
-	// first quadrant
+	// First quadrant
 	v0 = (*T).getVertex(startIndex);
 	start = new TEdge(v0, rv0);
 	(*T).addEdge(start);
@@ -165,16 +222,18 @@ void boxPolygon(Triangulation* T, int startIndex){
 		next = new TEdge(v1, rv0);
 		(*T).addEdge(next);
 
-		new Triangle(prev, (*v0).getEdgeTo(v1), next, v0, v1, rv0, "box polygon 1", ok);
+		new Triangle(prev, (*v0).getEdgeTo(v1), next, v0, v1, rv0);
 
 		v0 = v1;
 		prev = next;
 	}
 
-	// second quadrant
+	// Second quadrant
+
+	// Close the gap between first and second quadrant
 	next = new TEdge(v0, rv1);
 	(*T).addEdge(next);
-	new Triangle(prev, next, re0, v0, rv0, rv1, "box polygon 2", ok);
+	new Triangle(prev, next, re0, v0, rv0, rv1);
 	prev = next;
 
 	for(; i <= limit1; i++){
@@ -182,16 +241,18 @@ void boxPolygon(Triangulation* T, int startIndex){
 		next = new TEdge(v1, rv1);
 		(*T).addEdge(next);
 
-		new Triangle(prev, (*v0).getEdgeTo(v1), next, v0, v1, rv1, "box polygon 3", ok);
+		new Triangle(prev, (*v0).getEdgeTo(v1), next, v0, v1, rv1);
 
 		v0 = v1;
 		prev = next;
 	}
 
-	// third quadrant
+	// Third quadrant
+
+	// Close the gap between second and third quadrant
 	next = new TEdge(v0, rv2);
 	(*T).addEdge(next);
-	new Triangle(prev, next, re1, v0, rv1, rv2, "box polygon 4", ok);
+	new Triangle(prev, next, re1, v0, rv1, rv2);
 	prev = next;
 
 	for(; i <= limit2; i++){
@@ -199,16 +260,18 @@ void boxPolygon(Triangulation* T, int startIndex){
 		next = new TEdge(v1, rv2);
 		(*T).addEdge(next);
 
-		new Triangle(prev, (*v0).getEdgeTo(v1), next, v0, v1, rv2, "box polygon 5", ok);
+		new Triangle(prev, (*v0).getEdgeTo(v1), next, v0, v1, rv2);
 
 		v0 = v1;
 		prev = next;
 	}
 
-	// fourth quadrant
+	// Fourth quadrant
+
+	// Close the gap between third and fourth quadrant
 	next = new TEdge(v0, rv3);
 	(*T).addEdge(next);
-	new Triangle(prev, next, re2, v0, rv2, rv3, "box polygon 6", ok);
+	new Triangle(prev, next, re2, v0, rv2, rv3);
 	prev = next;
 
 	for(; i < Settings::initialSize; i++){
@@ -216,17 +279,17 @@ void boxPolygon(Triangulation* T, int startIndex){
 		next = new TEdge(v1, rv3);
 		(*T).addEdge(next);
 
-		new Triangle(prev, (*v0).getEdgeTo(v1), next, v0, v1, rv3, "box polygon 7", ok);
+		new Triangle(prev, (*v0).getEdgeTo(v1), next, v0, v1, rv3);
 
 		v0 = v1;
 		prev = next;
 	}
 
-	// close the triangulation
+	// Close the gap between the first and the fourth quadrant
 	v1 = (*T).getVertex(startIndex);
 	next = new TEdge(v1, rv3);
 	(*T).addEdge(next);
 
-	new Triangle(prev, (*v0).getEdgeTo(v1), next, v0, v1, rv3, "box polygon 8", ok);
-	new Triangle(next, start, re3, v1, rv0, rv3, "box polygon 9", ok);
+	new Triangle(prev, (*v0).getEdgeTo(v1), next, v0, v1, rv3);
+	new Triangle(next, start, re3, v1, rv0, rv3);
 }
