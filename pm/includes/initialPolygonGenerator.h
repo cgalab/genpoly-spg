1bd83f6a3c111684b8e6e6a433c7e397f1e194ec
Philipp Mayer <mayerph@stud.sbg.ac.at>
2020-01-23 12:24:41 +0100

    moved my test stuff to the thesis repo

--- pm/includes/initialPolygonGenerator.h
+++
@@ -1,9 +0,0 @@
-#include "point.h"
-#include "polygon.h"
-#include <stdlib.h>
-#include <math.h>
-
-enum class RPShapes {RPS_FLAT, RPS_QUADRATIC};
-
-Polygon* generateRectangularPolygon(int n, enum RPShapes type);
-Polygon* generateRegularPolygon(int n);
--- pm/includes/polygonTransformer.h
+++
@@ -1,9 +0,0 @@
-#include "polygon.h"
-#include "randomGenerator.h"
-#include <stdio.h>
-#include "edge.h"
-#include "point.h"
-#include "timer.h"
-
-void transformPolygon(Polygon* p, int iterations, Timer t);
-bool checkSimplicityOfTranslation(Polygon* p, int index);
